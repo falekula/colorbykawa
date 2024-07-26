ac0e88a5d66741d070ad61d823cce2cb908bf4fd
falekula <falekula1@gmail.com>
2024-07-26 19:43:35 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-from colorbykawa import ColorByKawa
+import colorbykawa
 
-col = ColorByKawa()
-print(col.Red + "This is red" + col.Reset)
 
+# Стандартные цвета
+print('{k.Red}This is red text{k.RESET}')
+
+# HEX цвет
+print('{k.HEX(#00FF00)}This is HEX green text{k.RESET}')
+
+# RGB цвет
+print('{k.RGB(0,0,255)}This is RGB blue text{k.RESET}')
